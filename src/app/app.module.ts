11d5353bb66e7b0336e03bbbaafc7c261d6d967e
OrtizIgnacioA <nacho.agus.ortiz@gmail.com>
2020-04-27 20:54:49 -0300

    Correcciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 
 // Primero registramos el modulo para formularios reactivos
-import {  FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { PersonaComponent } from './formularios/reactivos/persona.component';
-import { AnimalComponent } from './formularios/template-driven/animal.component';
+import { AppComponent } from "./app.component";
+import { PersonaComponent } from "./formularios/reactivos/persona.component";
+import { AnimalComponent } from "./formularios/template-driven/animal.component";
 
 // Declaramos e importamos el componente en app.module para que sea visible por la aplicación
-import { AlumnoComponent } from './formularios/reactivos/Alumno/alumno.component';
+import { AlumnoComponent } from "./formularios/reactivos/Alumno/alumno.component";
 
 @NgModule({
-	declarations: [ AppComponent, PersonaComponent, AnimalComponent, AlumnoComponent ],
-	imports: [ BrowserModule, AppRoutingModule, ReactiveFormsModule ], // Lo agregamos en imports para importar todas sus directivas
-	providers: [],
-	bootstrap: [ AppComponent ]
+  declarations: [
+    AppComponent,
+    PersonaComponent,
+    AnimalComponent,
+    AlumnoComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule], // Lo agregamos en imports para importar todas sus directivas
+  providers: [],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
